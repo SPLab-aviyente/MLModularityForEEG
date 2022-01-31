30f5d0401b49ef6c8e90076a0a52f57dd40878b1
abdkarr <abdullah.karaaslanli@gmail.com>
2022-01-31 16:55:59 -0500

    Add find null communities step to the runner.

--- individual_runner.py
+++ individual_runner.py
@@ -106,11 +106,9 @@ def find_communities(inputs, graphs):
         save_dir = os.path.join(output_dir, networks_dir, graph_name, "modularities")
         Path(save_dir).mkdir(parents=True, exist_ok=True)
 
-        now = datetime.now()
-        now_str = "{}{:02d}{:02d}{}{}".format(now.year, now.month, now.day, now.hour, now.minute)
-        save_name = "{}_gmin_{:.3f}_gmax_{:.3f}_ng_{:d}_omin_{:.3f}_omax_{:.3f}_no_{:d}_"\
-                    "{}_nruns_{:d}.csv".format(now_str, gamma_min, gamma_max, n_gammas, 
-                                               omega_min, omega_max, n_omegas, null_model, n_runs)
+        save_name = "gmin_{:.3f}_gmax_{:.3f}_ng_{:d}_omin_{:.3f}_omax_{:.3f}_no_{:d}_"\
+                    "nruns_{:d}/{}.csv".format(gamma_min, gamma_max, n_gammas, 
+                                               omega_min, omega_max, n_omegas, n_runs, null_model)
         save_file = os.path.join(save_dir, save_name)
         modularities_df.to_csv(save_file)
 
@@ -141,7 +139,7 @@ def create_null_networks(inputs, graphs):
             # n_runs number of null networks in this folder since we need enough samples to make 
             # any statictical conclusion. If not, we will create new nulls. 
 
-            # get gml and gziped files
+            # get gml and zipped gml files
             files = glob.glob(os.path.join(nulls_dir, "*.gml")) + \
                     glob.glob(os.path.join(nulls_dir, "*.gml.gz"))
 
@@ -174,8 +172,60 @@ def create_null_networks(inputs, graphs):
 
     return nulls
 
-def find_null_communities():
-    pass
+def find_null_communities(inputs, nulls):
+    output_dir = inputs["output_dir"]
+    networks_dir = inputs["networks_dir"]
+
+    # Get modularity parameters
+    gamma_min = inputs["gamma"][0]["min"]
+    gamma_max = inputs["gamma"][1]["max"]
+    n_gammas = inputs["gamma"][2]["n_points"]
+
+    omega_min = inputs["omega"][0]["min"]
+    omega_max = inputs["omega"][1]["max"]
+    n_omegas = inputs["omega"][2]["n_points"]
+
+    gammas = np.linspace(gamma_min, gamma_max, n_gammas, endpoint=True)
+    omegas = np.linspace(omega_min, omega_max, n_omegas, endpoint=True)
+
+    params = list(product(gammas, omegas))
+    null_model = inputs["null_model"]
+    n_params = len(params)
+
+    null_names = {net["network"]: net["null_networks_type"] for net in inputs["networks"]}
+
+    for graph_name, null_nets in nulls.items():
+        n_runs = len(null_nets)
+
+        modularities = np.zeros((n_runs, n_params))
+
+        for r, G in enumerate(null_nets):
+            # get edge weights and expected edge weights under the null model
+            w_intra, w_inter = modularity.get_edge_weights(G)
+            p_intra, p_inter = modularity.get_pijs(G, null_model)
+
+            # at this step, we only need modularity values of partitions
+            for p, param in enumerate(params):
+                gamma = param[0]
+                omega = param[1]
+
+                _, q = modularity.find_comms(G, w_intra, w_inter, p_intra, p_inter, gamma, omega)
+
+                modularities[r, p] = q
+
+        df_index = pd.MultiIndex.from_product([gammas, omegas], names=["gamma", "omega"])
+        modularities_df = pd.DataFrame(modularities, columns=df_index)
+
+        # save modularity values to csv
+        save_dir = os.path.join(output_dir, networks_dir, graph_name, "modularities")
+        Path(save_dir).mkdir(parents=True, exist_ok=True)
+
+        save_name = "gmin_{:.3f}_gmax_{:.3f}_ng_{:d}_omin_{:.3f}_omax_{:.3f}_no_{:d}_"\
+                    "nruns_{:d}/{}_{}.csv".format(gamma_min, gamma_max, n_gammas, omega_min, 
+                                                  omega_max, n_omegas, n_runs, null_model,
+                                                  null_names[graph_name])
+        save_file = os.path.join(save_dir, save_name)
+        modularities_df.to_csv(save_file)
 
 def select_params():
     pass
@@ -197,15 +247,15 @@ if __name__ == "__main__":
         print("There is no network to analyze, runner is aborted.")
         quit()
 
-    # find_communities(inputs, graphs) # find the communities of observed multilayer graphs
+    find_communities(inputs, graphs) # find the communities of observed multilayer graphs
 
-    create_null_networks(inputs, graphs) # create or read null networks
+    nulls = create_null_networks(inputs, graphs) # create or read null networks
 
-    find_null_communities()
+    find_null_communities(inputs, nulls)
 
-    select_params()
+    # select_params()
 
-    find_consensus_comms()
+    # find_consensus_comms()
 
-    if len(graphs) > 1:
-        find_group_comms()
+    # if len(graphs) > 1:
+    #     find_group_comms()
