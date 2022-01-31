12edc796781e2edda9012e9bfbfa9f15c560e02d
abdkarr <abdullah.karaaslanli@gmail.com>
2022-01-31 17:35:39 -0500

    Add parameter selection step to the runner.

--- individual_runner.py
+++ individual_runner.py
@@ -41,9 +41,12 @@ def read_networks(inputs):
     # File names (without extension) of observed networks
     network_names = [net["network"] for net in inputs["networks"]]
 
+    n_networks = len(network_names)
     graphs = {}
 
-    for network_name in network_names:
+    print("Reading multilayer networks: " + "_"*n_networks + "\r", end="")
+
+    for i, network_name in enumerate(network_names):
 
         network_path = os.path.join(input_dir, networks_dir, network_name)
 
@@ -58,6 +61,11 @@ def read_networks(inputs):
             graphs[network_name] = mlgraph.read.from_zipped_gml(network_path + ".gml.gz")
         else:
             print("File '{}' does not exist, skipping this network.".format(network_name))
+
+        print("Reading multilayer networks: " + "X"*(i+1) + "_"*(n_networks-i-1) + "\r", end="")
+
+    print()
+    print("Multilayer networks are read.")
 
     return graphs
 
@@ -83,8 +91,14 @@ def find_communities(inputs, graphs):
     n_params = len(params)
     n_runs = inputs["n_runs"]
 
+    iter = 0
+    n_iters = len(graphs)*n_params
+    percentage_done = iter/n_iters
+    print("Finding communities of the observed networks: {:.4f}\r".format(percentage_done), end="")
+
+    modularities = {}
     for graph_name, G in graphs.items():
-        modularities = np.zeros((n_runs, n_params))
+        modularities[graph_name] = np.zeros((n_runs, n_params))
 
         # get edge weights and expected edge weights under the null model
         w_intra, w_inter = modularity.get_edge_weights(G)
@@ -97,10 +111,15 @@ def find_communities(inputs, graphs):
 
             _, q = modularity.find_comms(G, w_intra, w_inter, p_intra, p_inter, gamma, omega, n_runs)
 
-            modularities[:, p] = q
+            modularities[graph_name][:, p] = q
+
+            iter += 1
+            percentage_done = iter/n_iters
+            print("Finding communities of the observed networks: {:.4f}".format(percentage_done), end="")
+
 
         df_index = pd.MultiIndex.from_product([gammas, omegas], names=["gamma", "omega"])
-        modularities_df = pd.DataFrame(modularities, columns=df_index)
+        modularities_df = pd.DataFrame(modularities[graph_name], columns=df_index)
 
         # save modularity values to csv
         save_dir = os.path.join(output_dir, networks_dir, graph_name, "modularities")
@@ -111,6 +130,11 @@ def find_communities(inputs, graphs):
                                                omega_min, omega_max, n_omegas, n_runs, null_model)
         save_file = os.path.join(save_dir, save_name)
         modularities_df.to_csv(save_file)
+
+    print()
+    print("Communities of the observed networks are found.")
+
+    return modularities
 
 def create_null_networks(inputs, graphs):
     output_dir = inputs["output_dir"]
@@ -125,13 +149,18 @@ def create_null_networks(inputs, graphs):
         "strength_and_layer_preserved": lambda G: wu_nulls.strength_preserved(G, preserve_layer=True)
     }
 
+    n_runs = inputs["n_runs"]
+
+    iter = 0
+    n_iters = len(graphs)*n_runs
+    percentage_done = iter/n_iters
+    print("Creating (or reading) null networks: {:.4f}\r".format(percentage_done), end="")
+
     nulls = {}
     for graph_name, G in graphs.items():
         
         null_name = null_names[graph_name]
         nulls_dir = os.path.join(output_dir, networks_dir, graph_name, null_name)
-
-        n_runs = inputs["n_runs"]
 
         create_nulls = True
         if os.path.isdir(nulls_dir): 
@@ -162,6 +191,10 @@ def create_null_networks(inputs, graphs):
                 file_name = os.path.join(nulls_dir, "network_{:d}".format(r+1))
                 mlgraph.write.to_zipped_gml(Gn, file_name)
 
+                iter += 1
+                percentage_done = iter/n_iters
+                print("Creating (or reading) null networks: {:.4f}\r".format(percentage_done), end="")
+
         else:
             # Read existing null networks
             for file in files:
@@ -169,6 +202,14 @@ def create_null_networks(inputs, graphs):
                     nulls[graph_name].append(mlgraph.read.from_gml(file))
                 elif file.endswith(".gml.gz"):
                     nulls[graph_name].append(mlgraph.read.from_gml(file))
+
+                if len(nulls) <= n_runs:
+                    iter += 1
+                percentage_done = iter/n_iters
+                print("Creating (or reading) null networks: {:.4f}\r".format(percentage_done), end="")
+
+    print()
+    print("Null networks are created (or read).")
 
     return nulls
 
@@ -194,10 +235,20 @@ def find_null_communities(inputs, nulls):
 
     null_names = {net["network"]: net["null_networks_type"] for net in inputs["networks"]}
 
+    iter = 0
+    
+    n_iters = len(nulls)*n_params
+    for _, null_nets in nulls.items():
+        n_iters *= len(null_nets)
+
+    percentage_done = iter/n_iters
+    print("Finding communities of the null networks: {:.4f}\r".format(percentage_done), end="")
+
+    modularities = {}
     for graph_name, null_nets in nulls.items():
         n_runs = len(null_nets)
 
-        modularities = np.zeros((n_runs, n_params))
+        modularities[graph_name] = np.zeros((n_runs, n_params))
 
         for r, G in enumerate(null_nets):
             # get edge weights and expected edge weights under the null model
@@ -211,10 +262,14 @@ def find_null_communities(inputs, nulls):
 
                 _, q = modularity.find_comms(G, w_intra, w_inter, p_intra, p_inter, gamma, omega)
 
-                modularities[r, p] = q
+                modularities[graph_name][r, p] = q
+
+                iter += 1
+                percentage_done = iter/n_iters
+                print("Finding communities of the null networks: {:.4f}\r".format(percentage_done), end="")
 
         df_index = pd.MultiIndex.from_product([gammas, omegas], names=["gamma", "omega"])
-        modularities_df = pd.DataFrame(modularities, columns=df_index)
+        modularities_df = pd.DataFrame(modularities[graph_name], columns=df_index)
 
         # save modularity values to csv
         save_dir = os.path.join(output_dir, networks_dir, graph_name, "modularities")
@@ -227,10 +282,49 @@ def find_null_communities(inputs, nulls):
         save_file = os.path.join(save_dir, save_name)
         modularities_df.to_csv(save_file)
 
-def select_params():
-    pass
+    print()
+    print("Communities of the null networks are found.")
 
-def find_consensus_comms():
+    return modularities
+
+def select_params(inputs, obs_modularities, null_modularities):
+    # Get modularity parameters
+    gamma_min = inputs["gamma"][0]["min"]
+    gamma_max = inputs["gamma"][1]["max"]
+    n_gammas = inputs["gamma"][2]["n_points"]
+
+    omega_min = inputs["omega"][0]["min"]
+    omega_max = inputs["omega"][1]["max"]
+    n_omegas = inputs["omega"][2]["n_points"]
+
+    gammas = np.linspace(gamma_min, gamma_max, n_gammas, endpoint=True)
+    omegas = np.linspace(omega_min, omega_max, n_omegas, endpoint=True)
+
+    params = list(product(gammas, omegas))
+
+    gamma_opts = {}
+    omega_opts = {}
+
+    for graph_name in obs_modularities:
+        # optimal gamma and omega are values where modularity of observed communities if maximally
+        # different than that of null communities
+        mod_diffs = np.mean(obs_modularities[graph_name], axis=0) - \
+                    np.mean(null_modularities[graph_name], axis=0)
+    
+        opt_indx = np.argmax(mod_diffs)
+        gamma_opt = params[opt_indx][0]
+        omega_opt = params[opt_indx][1]
+        mod_diff_opt = mod_diffs[opt_indx]
+
+        print("Best gamma and omega for {} are {:.4f}, {:.4f}; with modularity difference {:.4f}".\
+            format(graph_name, gamma_opt, omega_opt, mod_diff_opt))
+
+        gamma_opts[graph_name] = gamma_opt
+        omega_opts[graph_name] = omega_opt
+
+    return gamma_opts, omega_opts
+
+def find_consensus_comms(inputs, graphs, gamma_opts, omega_opts):
     pass
 
 def find_group_comms():
@@ -247,15 +341,19 @@ if __name__ == "__main__":
         print("There is no network to analyze, runner is aborted.")
         quit()
 
-    find_communities(inputs, graphs) # find the communities of observed multilayer graphs
+    # find the communities of observed multilayer graphs
+    obs_modularities = find_communities(inputs, graphs) 
 
-    nulls = create_null_networks(inputs, graphs) # create or read null networks
+    # create or read null networks
+    nulls = create_null_networks(inputs, graphs) 
 
-    find_null_communities(inputs, nulls)
+    # find the communities of the null networks
+    null_modularities = find_null_communities(inputs, nulls)
 
-    # select_params()
+    # select the optimal gamma and omega
+    gamma_opts, omega_opts = select_params(inputs, obs_modularities, null_modularities)
 
-    # find_consensus_comms()
+    find_consensus_comms(inputs, graphs, gamma_opts, omega_opts)
 
     # if len(graphs) > 1:
     #     find_group_comms()
